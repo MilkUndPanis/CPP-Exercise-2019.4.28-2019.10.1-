1c570979572c09d1dd578279502af166e856ef89
Mare Magnum <47765769+MilkUndPanis@users.noreply.github.com>
2020-07-11 22:31:41 +0800

    Create MustDefine.cpp

---
+++ MustDefine.cpp
@@ -0,0 +1,63 @@
+//"_mustdefine_.h"
+#include<iostream>
+#define _NSP_DEF_ namespace
+#define _BEGIN {
+#define _END }
+#define _STRU_END };
+#define _REF_NSP using namespace
+#define _MAIN_BEGIN_ int main()
+#define _IS_EQUAL_TO_ =
+#define _SHORTER_VER__ short
+#define _MAKE_THE_INTERGER_ int
+#define _MAKE_LONG_INTERGER long 
+#define _MAKE_LONGER_INTERGER long long
+#define _POSITIVELY_ unsigned
+#define _RETURN_TO_SYSTEM_ return 0;
+#define _OUTPUT_STREAM_COMING_OUT cout<<
+#define _END_A_LINE <<endl;
+#define _MAKE_THE_FLOAT_ float 
+#define _MAKE_THE_DOUBLE_FLOAT_ double
+#define _MAKE_THE_LARGER_FLOAT long double
+#define TRUE_OR_FALSE bool
+#define _USING_CHARACTER_ char
+#define _TRUE_ 1
+#define _FALSE_ 0
+#define LEFT__ (
+#define __RIGHT )
+#define _AND_ ,
+#define _NOTHING_ELSE_ void
+#define _A_STRUCT_IS_ struct
+#define _A_OBJECT_IS_ class
+#define _END_OF_A_SENTENCE_ ;
+#define _CONSTANCE_ const
+#define _COMING_IN_INPUT_STREAM_ cin>>
+#define _OF_ [
+#define _ELEMENTS_ ]
+#define _SOLVE_ *
+#define _ADDRESS_ &
+#define _ADD_UP_WITH_ +
+#define _SUB_WITH_ -
+#define _MULTI_ *
+#define _MINUS_ /
+#define _MOD_ %
+#define _DIVIDE_ <<
+#pragma once
+
+#include<iostream>
+#include "_mustdefine_.h"
+_NSP_DEF_ two _BEGIN
+	_CONSTANCE_ _MAKE_THE_INTERGER_ TWO _IS_EQUAL_TO_ 2 _END_OF_A_SENTENCE_
+_END
+_MAIN_BEGIN_
+_BEGIN
+	_REF_NSP std _END_OF_A_SENTENCE_
+	_REF_NSP two _END_OF_A_SENTENCE_
+	_MAKE_THE_DOUBLE_FLOAT_ ar _OF_ 5 _ELEMENTS_  _IS_EQUAL_TO_
+	_BEGIN 5.1 _AND_ 4.5 _AND_ 3.3 _AND_ 3.4 _AND_ 1.5 _END _END_OF_A_SENTENCE_
+	_MAKE_THE_INTERGER_ THREE _IS_EQUAL_TO_ 3 _END_OF_A_SENTENCE_
+	_OUTPUT_STREAM_COMING_OUT TWO _END_A_LINE _END_OF_A_SENTENCE_
+	_OUTPUT_STREAM_COMING_OUT THREE _END_A_LINE _END_OF_A_SENTENCE_
+	_OUTPUT_STREAM_COMING_OUT _SOLVE_ ar _ADD_UP_WITH_ LEFT__ 3 __RIGHT _DIVIDE_
+	" is a float number." _END_A_LINE _END_OF_A_SENTENCE_
+	_RETURN_TO_SYSTEM_
+_END
